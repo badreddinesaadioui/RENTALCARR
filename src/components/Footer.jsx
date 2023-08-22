bbad0e5b8cfad2c44832d76663afdac305755d7f
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-08-22 19:11:48 +0100

    Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,18 +13,18 @@ function Footer() {
                 have the perfect car to meet your needs.
               </li>
               <li>
-                <a href="tel:123456789">
-                  <i className="fa-solid fa-phone"></i> &nbsp; (123) -456-789
+                <a href="tel:0687301564">
+                  <i className="fa-solid fa-phone"></i> &nbsp; (+212) 687-301-564
                 </a>
               </li>
 
               <li>
                 <a
                   href="mailto: 
-                carrental@gmail.com"
+                growstudio.ma@gmail.ma"
                 >
                   <i className="fa-solid fa-envelope"></i>
-                  &nbsp; calebjephuneh@gmail.com
+                  &nbsp; growstudio.ma@gmail.com
                 </a>
               </li>
 
@@ -32,50 +32,50 @@ function Footer() {
                 <a
                   style={{ fontSize: "14px" }}
                   target="_blank"
-                  rel="noreferrer"
-                  href="https://jephunneh.com/"
+                  rel="growstudio"
+                  href="https://grows.studio"
                 >
-                  Design by cjephuneh
+                  Visitez notre site grows.studio
                 </a>
               </li>
             </ul>
 
             <ul className="footer-content__2">
-              <li>Company</li>
+              <li>Entreprise</li>
               <li>
-                <a href="#home">New York</a>
+                <a href="#home">A-propos</a>
               </li>
               <li>
-                <a href="#home">Careers</a>
+                <a href="#home">Modèles de voitures</a>
               </li>
               <li>
                 <a href="#home">Mobile</a>
               </li>
               <li>
-                <a href="#home">Blog</a>
+                <a href="#home">Attestations</a>
               </li>
               <li>
-                <a href="#home">How we work</a>
+                <a href="#home">Notre équipe</a>
               </li>
             </ul>
 
             <ul className="footer-content__2">
-              <li>Working Hours</li>
-              <li>Mon - Fri: 9:00AM - 9:00PM</li>
-              <li>Sat: 9:00AM - 19:00PM</li>
-              <li>Sun: Closed</li>
+              <li>Heures de Travail</li>
+              <li>Lun - Ven : 9h00 - 21h00</li>
+              <li>Sam : 9h00 - 19h00</li>
+              <li>Dim : Fermé</li>
             </ul>
 
             <ul className="footer-content__2">
-              <li>Subscription</li>
+              <li>Abonnement</li>
               <li>
-                <p>Subscribe your Email address for latest news & updates.</p>
+                <p>Abonnez-vous avec votre adresse e-mail pour les dernières actualités et mises à jour.</p>
               </li>
               <li>
                 <input type="email" placeholder="Enter Email Address"></input>
               </li>
               <li>
-                <button className="submit-email">Submit</button>
+                <button className="submit-email">Envoyer</button>
               </li>
             </ul>
           </div>

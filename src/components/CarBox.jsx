74d36ea36a75cfbc3ebfc9556cf2dfa3db033a5f
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-08-22 19:28:55 +0100

    Update CarBox.jsx

--- src/components/CarBox.jsx
+++ src/components/CarBox.jsx
@@ -19,31 +19,32 @@ function CarBox({ data, carID }) {
           {/* description */}
           <div className="pick-description">
             <div className="pick-description__price">
-              <span>${car.price}</span>/ rent per day
+              <span>${car.price}</span>/ location par jour
             </div>
             <div className="pick-description__table">
               <div className="pick-description__table__col">
-                <span>Model</span>
+                <span>Modèle</span>
                 <span>{car.model}</span>
               </div>
 
               <div className="pick-description__table__col">
-                <span>Mark</span>
+                <span>Marque</span>
                 <span>{car.mark}</span>
               </div>
 
+              
               <div className="pick-description__table__col">
-                <span>Year</span>
+                <span>Année</span>
                 <span>{car.year}</span>
               </div>
 
               <div className="pick-description__table__col">
-                <span>Doors</span>
+                <span>Portes</span>
                 <span>{car.doors}</span>
               </div>
 
               <div className="pick-description__table__col">
-                <span>AC</span>
+                <span>Climatisation</span>
                 <span>{car.air}</span>
               </div>
 
@@ -53,13 +54,13 @@ function CarBox({ data, carID }) {
               </div>
 
               <div className="pick-description__table__col">
-                <span>Fuel</span>
+                <span>Carburant</span>
                 <span>{car.fuel}</span>
               </div>
             </div>
             {/* btn cta */}
             <a className="cta-btn" href="#booking-section">
-              Reserve Now
+              Réserver Maintenant
             </a>
           </div>
         </div>
